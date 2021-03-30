7d2342127ff4507111e6d902671ae95c5796ca87
Rachel Sharp <rachel@hashicorp.com>
2021-03-30 11:30:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -104,3 +104,8 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.example.public_ip
 }
+
+output "instance_name" {
+  description = "Tags of the EC2 instance"
+  value       = aws_instance.example.tags
+}
