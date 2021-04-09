261231df4c66cd8570e4c8822405b53ca07dcb36
Rachel Sharp <rachel@hashicorp.com>
2021-04-08 22:56:07 -0400

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
+
+output "instance_name" {
+  description = "Tags of the EC2 instance"
+  value       = aws_instance.example.tags
+}
