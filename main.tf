c7f84ef1be72dc6f39e163737743e604451aed4d
Rachel Sharp <rachel@hashicorp.com>
2021-04-12 19:17:18 -0400

    update instance name

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "web_app" {
   for_each               = aws_security_group.*.id
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.example.id
+  value       = aws_instance.web_app.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.example.public_ip
+  value       = aws_instance.web_app.public_ip
 }
 
 output "instance_name" {
   description = "Tags of the EC2 instance"
-  value       = aws_instance.example.tags
+  value       = aws_instance.web_app.tags
 }
