24e595df357f541482629e248a02d770ce3801eb
Tu Nguyen <im2nguyen@gmail.com>
2022-03-07 12:09:00 -0800

    Update comment

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_security_group" "sg_8080" {
     protocol        = "tcp"
     security_groups = [aws_security_group.sg_ping.id]
   }
-  // required to update apt-get
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
     to_port     = 0
